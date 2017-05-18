11323e495dcfb7a5e3835b8599a12c4612137c27
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-05-18 16:44:23 +0200

    Add skeleton for separate resolutions

---
+++ NUTS_resolutions/scripts/separate_nuts_level2.sql
@@ -0,0 +1,10 @@
+﻿SELECT *
+INTO eurostat.nuts_rg_01m_2013_level2
+FROM eurostat.nuts_rg_01m_2013
+WHERE stat_levl_ = 2;
+
+-- Define a primary key
+ALTER TABLE eurostat.nuts_rg_01m_2013_level2 ADD PRIMARY KEY (gid);
+
+-- Spatially enable it
+SELECT Populate_Geometry_Columns('eurostat.nuts_rg_01m_2013_level2'::regclass);
